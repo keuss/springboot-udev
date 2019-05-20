8bd46b26cc6632b9bb220f975f1271a67c1aeb30
keuss <guillaume.gallois@gmail.com>
2019-05-20 11:53:42 +0200

    add sql data

--- src/main/java/com/example/demo/pojo/Book.java
+++ src/main/java/com/example/demo/pojo/Book.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotEmpty;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty
---
+++ src/main/resources/db/runtime.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.book(id, title, author) VALUES (0, 'LOTR 1', 'JR Tolkien');
+INSERT INTO public.book(id, title, author) VALUES (1, 'LOTR 2', 'JR Tolkien');
+INSERT INTO public.book(id, title, author) VALUES (2, 'LOTR 3', 'JR Tolkien');
